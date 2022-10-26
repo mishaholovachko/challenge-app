49e65ca6b392d57ab30d7c94f84ba033e645b1fa
Misha Holovachko <misha.h@otakoyi.com>
2022-10-26 12:11:12 +0300

    Create filter items view

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,35 @@
 import React, { ChangeEvent } from "react";
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Chip, CssBaseline, Grid, Stack, ThemeProvider } from "@mui/material";
 
 import Search from "./components/Search/Search";
+import { FILTER_ITEMS } from "./mockData";
 
 import { theme } from "./muiTheme";
 
 function App() {
 
   const handleSearchData = (event: ChangeEvent<HTMLInputElement>) => {
-  console.log('event:', event)
+    console.log('event:', event)
+  }
+
+  const handleFilter = (value: string) => {
+    console.log('value:', value)
   }
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Grid container alignItems="center">
         <Search handleSearch={handleSearchData}/>
+
+        <Stack direction="row" spacing={1}>
+          {
+            FILTER_ITEMS.map((item) => (
+              <Chip label={item.title} variant="outlined" onClick={() => handleFilter(item.value)} />
+            ))
+          }
+        </Stack>
+        </Grid>
     </ThemeProvider>
   );
 }
---
+++ src/mockData.ts
@@ -0,0 +1,18 @@
+export const FILTER_ITEMS = [
+    {
+      title: 'Agent',
+      value: 'agent'
+    },
+    {
+      title: 'Shop',
+      value: 'shop'
+    },
+    {
+      title: 'Property',
+      value: 'property'
+    },
+    {
+      title: 'All',
+      value: 'all'
+    },
+  ]
